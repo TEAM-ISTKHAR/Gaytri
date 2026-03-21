9b6ff86595252b6217196bd89528df8836e0ba47
rockyxd3 <shivamlkr629@gmail.com>
2026-03-21 22:57:27 +0530

    Update queue.py

--- MecoMusic/utils/inline/queue.py
+++ MecoMusic/utils/inline/queue.py
@@ -72,14 +72,25 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text=">>I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(
-                text="▢",
-                callback_data=f"ADMIN Stop|{chat_id}",
-                style=ButtonStyle.DANGER,
-            ),
+           # InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+           # InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+           # InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+           # InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+        [
+            InlineKeyboardButton(
+                text="α∂∂ мє",
+                url="https://t.me/ChaahatMusicBot?startgroup=true",
+                style=ButtonStyle.SUCCESS,
+                
+            ),
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data="close",
+                style=ButtonStyle.DANGER,
+            
+            ),
+        ]
     ]
+    
     return buttons
